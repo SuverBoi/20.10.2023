2afcf1c89e2be7ef8f8bfe38da3be1b69715fe1f
SuverBoi <dmitro.kovalik12345@gmail.com>
2023-10-20 22:20:23 +0300

    l

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,77 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    std::cout << "Hello World!\n";
+
+
+
+    float f = 5.25213412;
+    double d = 5.432423423;
+
+    cout << f << endl;
+    cout << d << endl;
+
+    /*char c = 'a';
+    string str = "lqwrwrwrwrwrw";
+    cout << "---------------------------------" << endl;
+    cout << "| Name | Weight | Age | Brother |" << endl;
+    cout << "---------------------------------" << endl;
+    cout << "|Cody   |   180.2 |  40 |John   |" << endl;
+    cout << "|David  |   175.3 |  38 |Andrew |" << endl;
+    cout << "|Robert |   140.3 |  27 |Fande  |" << endl;
+    cout << "---------------------------------" << endl;
+
+
+    double  items = 3;
+    double  pricee = 50;
+    string name = "Machine";
+    cin >> items;
+    cin >> pricee;
+    cin >> name;
+
+    cout << "You ordered " << items << " " << name << " at $" << pricee << " each";
+
+    double a = 1;
+    double b = 1;
+
+    cin >> a;
+    cin >> b;
+
+    cout << "S=" << a * b << "";
+
+    double p = 3.14;
+    double r = 1;
+    double h = 1;
+    cin >> r;
+    cin >> h;
+
+    cout << "S=" << p * r * r * h << "";
+
+
+    string nameN = "pizza";
+    double price = 1;
+    double size = 1;
+    double delieverly = 1;
+    cin >> nameN;
+    cin >> price;
+    cin >> size;
+    cin >> delieverly;
+    cout << "---------------------------------" << endl;
+    cout << "| Name | Price | Size | Deliever |" << endl;
+    cout << "---------------------------------" << endl;
+    cout << "| " << nameN << " | " << price / 100 * 120 << "$ |  " << size << " | " << delieverly << " |" << endl;
+    cout << "---------------------------------" << endl;*/
+
+
+    int UAN = 1;
+    int UANkopiyka = 1;
+
+    cin >> UAN;
+    cin >> UANkopiyka;
+    double sum = UANkopiyka % 100;
+    cout << sum <<;
+}
